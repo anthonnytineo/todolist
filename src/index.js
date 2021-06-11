e5cb87c9c19c16c2014337480d975ac38cfb15c8
Anthonny Tineo Ruiz <anthonny>
2021-06-11 00:16:58 -0400

    mejora de funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
